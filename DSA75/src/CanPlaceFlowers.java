33f6b3a26f06fc38eca6a4a705d6a260cfc3b8de
Aman Kumar Singh <apkaman9@gmail.com>
2023-10-02 18:50:45 +0530

    Update CanPlaceFlowers.java

--- DSA75/src/CanPlaceFlowers.java
+++ DSA75/src/CanPlaceFlowers.java
@@ -16,19 +16,13 @@ public class CanPlaceFlowers {
 	         if(n==1 && flowerbed.length==1){
 	             return flowerbed[0]==0;
 	         }
-	         for(int i=0;i<flowerbed.length;i++){
+	         for(int i=1;i<flowerbed.length-1;i++){
 	             if(flowerbed[i]==1){
 	 	                 continue;
 	 	             }
-	 	             if(i==0   && flowerbed[i+1]!=1 ){
+	 	             if( flowerbed[i-1]!=1 && flowerbed[i+1]!=1){
 	 	                 n--;
-	 	                 flowerbed[i]=-1;
-	 	             } else if(i==flowerbed.length-1 && flowerbed[i-1]!=1 && flowerbed[i-1]!=-1){
-	 	                 n--;
-	 	                 flowerbed[i]=-1;
-	 	             }else if(i>0 && i<flowerbed.length-1 && flowerbed[i-1]!=-1 && flowerbed[i-1]!=1 && flowerbed[i+1]!=1){
-	 	                 n--;
-	                      flowerbed[i]=-1;
+	                      flowerbed[i]=1;
 	 	             }
 
 	 	             if(n==0){
